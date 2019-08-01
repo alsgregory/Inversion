79aad281be7d600dfbf8f590a1df99f9c86861be
Alastair Gregory <alsgregory@aol.com>
2019-08-01 11:26:09 +0100

    refactor

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Inversion',
+    description='Implementation of MCMC and Particle Filter Bayesian Model Calibration (Multi-output Gaussian Processes)',
+    author='Alastair C. A. Gregory',
+    author_email='alsgregory12@gmail.com',
+    LICENSE='MIT',
+    packages=['inversion'])
